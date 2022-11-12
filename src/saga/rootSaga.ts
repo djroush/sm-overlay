e2ad0e1dd06a368689ba6524caab5324585b892c
djroush <39247445+djroush@users.noreply.github.com>
2022-11-12 23:02:09 +0000

    Deploying to main from @ djroush/sm-overlay-generator@722d50935df01bdf37f080aaf246de0e69759033 🚀

--- src/saga/rootSaga.ts
+++
@@ -1,9 +0,0 @@
-import { spawn } from 'redux-saga/effects'
-import watchInformation from './Information';
-import watchTheme from './Theme';
-
-export default function* rootSaga() {
-    yield spawn(watchTheme);
-    yield spawn(watchInformation);
-}
-
