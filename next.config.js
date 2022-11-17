d794febda65346789718292c092cf1e3764f5863
Doug Roush <djroush@gmail.com>
2022-11-17 09:16:27 -0700

    Fix favicon.ico url when deployed

--- next.config.js
+++ next.config.js
@@ -3,14 +3,16 @@
 const isGithubActions = process.env.GITHUB_ACTIONS || false
 
 let assetPrefix = '/'
-
+let basePath = ''
 if (isGithubActions) {
   // trim off `<owner>/`
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
   assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
 }
 
 const nextConfig = {
+  basePath: basePath,
   exportPathMap: function () {
     return {
       "/": { page: "/" }
